4a89f0131be40d44d74161220b62d0ebf96494e2
Akos Kemives <akoskm@gmail.com>
2016-11-12 08:07:23 +0100

    update header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,8 @@ const Header = (props) =>
       nicepls
     </div>
     <div style={style.intro}>
-      nicepls mimics the original alexjs <a href='http://alexjs.com/#demo'>online demo</a>,
+      nicepls detects insensitive words, explains the errors and suggests possible alternatives.
+      It mimics the original alexjs <a href='http://alexjs.com/#demo'>online demo</a>,
       read the motivation <a href='https://github.com/akoskm/nicepls#motivation'>here</a>.
     </div>
   </div>
